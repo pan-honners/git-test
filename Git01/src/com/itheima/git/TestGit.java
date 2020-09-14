7ba1d69d11e318a2fb7ac9e36f7bc4964b2d2d92
admin <admin@.com>
2020-09-14 20:44:05 +0800

    第一次用idea把本地工程添加到git

---
+++ Git01/src/com/itheima/git/TestGit.java
@@ -0,0 +1,24 @@
+package com.itheima.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+        System.out.println("this is a git program");
+
+    }
+    public void test(){
+
+        int a=10;
+        a+=10;
+        System.out.println(a);
+
+    }
+}
